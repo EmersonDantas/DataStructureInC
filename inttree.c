e612474a85c4bb939af3f35eef91652710c0245d
waldirmarques <waldirmarquesdesouza@gmail.com>
2019-02-14 16:24:20 -0300

    Implementação até a função intnode_search

--- inttree.c
+++ inttree.c
@@ -1,4 +1,50 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#include "intree.h"
+#include "Inttree.h"
+
+void inttree_int(IntTree **tree)
+{
+    (*tree) = (IntTree*) malloc(sizeof(IntTree));
+    (*tree) -> root = NULL;
+    (*tree) -> size = 0;
+    return;
+}
+
+IntTreeNode* intnode_init(int val)
+{
+    IntTreeNode *z;
+
+    z = (IntTreeNode*) malloc(sizeof(IntTreeNode));
+
+    z -> data = val;
+    z -> father = NULL;
+    z -> left = NULL;
+    z -> right = NULL;
+
+    return z;
+}
+
+void preorder(IntTreeNode *node)
+{
+    if(node != NULL){
+        printf("%5d", node -> data);
+        preorder(node -> left);
+        preorder(node -> right);
+    }
+}
+
+void inorder(IntTreeNode *node)
+{
+    if(node != NULL){
+        inorder(node -> left);
+        printf("%5d", node -> data);
+        inorder(node -> right);
+    }
+}
+
+IntTreeNode* intnode_search(IntTree *tree, int val)
+{
+    return intnode_search(tree -> root, val);
+}
+
