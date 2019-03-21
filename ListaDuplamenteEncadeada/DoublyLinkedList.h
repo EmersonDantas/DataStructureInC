f3d2d8da1ebb364842bbe3266d021bc16338b464
PedroCarl0s <pedro.carlos@dce.ufpb.br>
2019-03-21 01:46:14 -0300

    remoção das descrições dos métodos (gerava duplicata na página web)

--- ListaDuplamenteEncadeada/DoublyLinkedList.h
+++ ListaDuplamenteEncadeada/DoublyLinkedList.h
@@ -21,10 +21,7 @@
 *   Typedefs
 ******************************************/
 
-/**
-* @brief Typedef utilizado para definir o pseudotipo "String" como
-* um ponteiro de char
-*/
+
 typedef char *String;
 
 /**
@@ -64,51 +61,16 @@ node_t *init_node(char *name, int temp);
 */
 node_t *init_lista();
 
-/**
-* @brief Protótipo para inserir no início da Lista Duplamente Encadeada com Sentinela
-* @param name - Ponteiro do nome da Cidade
-* @param temp - Temperatura da Cidade
-* @return void
-*
-*/
 void push_front(char *name, int temp);
 
-/**
-* @brief Protótipo para inserir no fim da Lista Duplamente Encadeada com Sentinela
-* @param name - Ponteiro do nome da Cidade
-* @param temp - Temperatura da Cidade
-* @return void
-*
-*/
 void push_back(char *name, int temp);
 
-/**
-* @brief Protótipo para remover no fim da Lista Duplamente Encadeada com Sentinela
-* @return void
-*
-*/
 void popFront();
 
-/**
-* @brief Protótipo para remover no início da Lista Duplamente Encadeada com Sentinela
-* @return void
-*
-*/
 void popLast();
 
-/**
-* @brief Protótipo para imprimir a Lista Duplamente Encadeada com Sentinela
-* @return void
-*
-*/
 void print_list();
 
-/**
-* @brief Protótipo para buscar a cidade pelo nome, na Lista Duplamente Encadeada com Sentinela
-* @param name - Ponteiro do nome da Cidade
-* @return void
-*
-*/
 void findName(char *name);
 
 #endif
